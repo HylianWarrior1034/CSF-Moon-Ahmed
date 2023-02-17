183ec51ad1b37961209b3758bca7f09a0ee958bc
HylianWarrior1034 <dm10142@gmail.com>
2023-02-17 12:23:52 -0500

    comments + main function

--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -10,12 +10,13 @@ int main(void)
   char asciival[3];
   char space[2] = {' ', '\0'};
   char newline[2] = {'\n', '\0'};
+  char colon[3] = {':', ' ', '\0'};
   unsigned nread = hex_read(data_buf);
   while (nread > 0)
   {
     hex_format_offset(offset, offset2);
     hex_write_string(offset2);
-    hex_write_string(': ');
+    hex_write_string(colon);
 
     for (int i = 0; i < nread; i++)
     {
@@ -25,7 +26,7 @@ int main(void)
     }
     hex_write_string(space);
     hex_write_string(data_buf);
-    hex_write_string('\n');
+    hex_write_string(newline);
     nread = hex_read(data_buf);
     offset += 16;
   }
