ec6ab40604ff5fd3b5b7052dd165437f027d5d93
HylianWarrior1034 <dm10142@gmail.com>
2023-03-28 01:26:28 -0400

    auto runner complete

--- csf_assign03/auto.py
+++ csf_assign03/auto.py
@@ -1,6 +1,20 @@
 import subprocess
 
-bashCommand = "CSF-Moon-Ahmed\csf_assign03\csim 256 4 16 write-allocate write-back lru < CSF-Moon-Ahmed\csf_assign03\gcc.trace"
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
-print(output)
+strategy = ["write-allocate write-back", "no-write-allocate write-through"]
+evict = ["lru", "fifo"]
+sizes = ["4", "256"]
+mapping = ["1024 1", "256 4", "1 1024"]
+
+for s in strategy:
+    for e in evict:
+        for size in sizes:
+            for m in mapping:
+                bashCommand = f"./csim {m} {size} {s} {e}"
+                myinput = open("gcc.trace")
+                process = subprocess.Popen(bashCommand.split(), stdin=myinput, stdout=subprocess.PIPE)
+                output, error = process.communicate()
+                print(f"{m} {size} {s} {e}")
+                print(output.decode("utf-8"))
+                print()
+                
+
