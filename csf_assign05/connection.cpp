cf4529d12822dade15505be1c77d5bb46268c7d2
HylianWarrior1034 <dm10142@gmail.com>
2023-04-20 01:28:03 -0400

    added receiver mistakes for connection

--- csf_assign05/connection.cpp
+++ csf_assign05/connection.cpp
@@ -23,10 +23,11 @@ void Connection::connect(const std::string &hostname, int port)
   // TODO: call open_clientfd to connect to the server
   // TODO: call rio_readinitb to initialize the rio_t object
   int client_fd = open_clientfd(hostname.c_str(), std::to_string(port).c_str());
-  if (client_fd < 0) {
+  if (client_fd < 0)
+  {
     std::cerr << "Could not connect to server" << std::endl;
     exit(1);
-  } 
+  }
   // handle error if client_fd is -1 or -2
   rio_readinitb(&m_fdbuf, client_fd);
 }
@@ -40,7 +41,8 @@ Connection::~Connection()
 bool Connection::is_open() const
 {
   // TODO: return true if the connection is open
-  if (m_last_result == EOF_OR_ERROR) {
+  if (m_last_result == EOF_OR_ERROR)
+  {
     return false;
   }
   return true;
@@ -80,19 +82,27 @@ bool Connection::receive(Message &msg)
   // TODO: receive a message, storing its tag and data in msg
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
-  char buf[1000];
-  ssize_t result = rio_readlineb(&m_fdbuf, buf, sizeof(buf));
-  if (result == -1)
+  std::string tag, data;
+  char buf[Message::MAX_LEN];                                 // create buffer
+  ssize_t n = rio_readlineb(&m_fdbuf, buf, Message::MAX_LEN); // read in message
+  if (n <= 0)
   {
-    msg.tag = TAG_ERR;
+    msg.tag = TAG_EMPTY;
     m_last_result = EOF_OR_ERROR;
-    return false;
   }
   else
   {
-    msg.tag = TAG_OK;
-    msg.data = buf;
-    m_last_result = SUCCESS;
+    int index = message.find(":");        // parse the message
+    msg.data = message.substr(index + 1); // before colon is tag
+    msg.tag = message.substr(0, index);   // after colon is message
+    if (msg.tag != TAG_ERR && msg.tag != TAG_OK && msg.tag != TAG_SLOGIN && msg.tag != TAG_RLOGIN && msg.tag != TAG_JOIN && msg.tag != TAG_LEAVE &&
+        msg.tag != TAG_SENDALL && msg.tag != TAG_SENDUSER && msg.tag != TAG_QUIT && msg.tag != TAG_DELIVERY && msg.tag != TAG_EMPTY)
+    {
+      m_last_result = INVALID_MSG; // check tag of message
+      return false;
+    }
+    m_last_result = SUCCESS; // change m_last_result to success if message is valid format
     return true;
   }
+  return false;
 }
