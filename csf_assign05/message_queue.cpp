a7eab0b3b210779e722bb57fa3f55e2a1da3321f
Ihsan Ahmed <iahmed6@jhu.edu>
2023-04-30 12:27:01 -0400

    Fixed errors tin message_queue.cpp

--- csf_assign05/message_queue.cpp
+++ csf_assign05/message_queue.cpp
@@ -6,22 +6,22 @@ MessageQueue::MessageQueue()
 {
   // TODO: initialize the mutex and the semaphore
   pthread_mutex_init(&m_lock, NULL);
-  sem_init(&m_avail)
+  sem_init(&m_avail, 0, 0);
 }
 
 MessageQueue::~MessageQueue()
 {
   // TODO: destroy the mutex and the semaphore
   pthread_mutex_destroy(&m_lock);
-  sem_destroy(&m_avail)
+  sem_destroy(&m_avail);
 }
 
 void MessageQueue::enqueue(Message *msg)
 {
   // TODO: put the specified message on the queue
-  Guard(&m_lock);
+  // Guard(&m_lock);
   m_messages.push_back(msg);
-  sem_post();
+  sem_post(&m_avail);
 
   // be sure to notify any thread waiting for a message to be
   // available by calling sem_post
@@ -61,7 +61,8 @@ Message *MessageQueue::dequeue()
       return msg;
     }
     // TODO: remove the next message from the queue, return it
-    Message *msg = m_messages.pop_front();
+    Message *msg = m_messages.front();
+    m_messages.pop_front();
     return msg;
   }
 }
