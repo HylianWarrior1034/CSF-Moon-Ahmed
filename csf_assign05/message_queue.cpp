e0c39ad1ec5247504be415c5c1740cf2b5dbe364
HylianWarrior1034 <dm10142@gmail.com>
2023-05-01 09:21:09 -0400

    message

--- csf_assign05/message_queue.cpp
+++ csf_assign05/message_queue.cpp
@@ -33,38 +33,39 @@ void MessageQueue::enqueue(Message *msg)
     // TODO: put the specified message on the queue
     Guard guard(m_lock);
     m_messages.push_back(msg);
-    sem_post(&m_avail);
   }
 
+  sem_post(&m_avail);
   // be sure to notify any thread waiting for a message to be
   // available by calling sem_post
 }
 
 Message *MessageQueue::dequeue()
 {
+
+  struct timespec ts;
+
+  // get the current time using clock_gettime:
+  // we don't check the return value because the only reason
+  // this call would fail is if we specify a clock that doesn't
+  // exist
+  clock_gettime(CLOCK_REALTIME, &ts);
+
+  // compute a time one second in the future
+  ts.tv_sec += 1;
+
+  // TODO: call sem_timedwait to wait up to 1 second for a message
+  //       to be available, return nullptr if no message is available
+  if (sem_timedwait(&m_avail, &ts) == -1)
+  {
+    return nullptr;
+  }
+
   {
     Guard guard(m_lock);
-    struct timespec ts;
-
-    // get the current time using clock_gettime:
-    // we don't check the return value because the only reason
-    // this call would fail is if we specify a clock that doesn't
-    // exist
-    clock_gettime(CLOCK_REALTIME, &ts);
-
-    // compute a time one second in the future
-    ts.tv_sec += 1;
-
-    // TODO: call sem_timedwait to wait up to 1 second for a message
-    //       to be available, return nullptr if no message is available
-    if (sem_timedwait(&m_avail, &ts) == -1)
-    {
-      return nullptr;
-    }
+    // TODO: remove the next message from the queue, return it
+    Message *msg = m_messages.front();
+    m_messages.pop_front();
   }
-  pthread_mutex_lock(&m_lock);
-  // TODO: remove the next message from the queue, return it
-  Message *msg = m_messages.front();
-  m_messages.pop_front();
   return msg;
 }
